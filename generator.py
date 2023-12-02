b7fd5dfe323aa1897c6bdae1047524657794e482
Umar <132471747+UMARMUKTHAR03@users.noreply.github.com>
2023-12-02 23:18:29 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -4,7 +4,6 @@ import tkinter as tk
 
 
 def generate_password():
-    # Get the length from the entry, set default to 12 if empty
     length = length_entry.get()
     if not length:
         length = 12
@@ -36,24 +35,20 @@ def generate_password():
     password_entry.insert(tk.END, password)
 
 
-# Create the GUI
 window = tk.Tk()
 window.title("Random Password Generator")
 
 window.configure(bg="#f2f2f2")
 
-# Resize and adapt widgets
 window.grid_columnconfigure(0, weight=1)
 window.grid_columnconfigure(1, weight=1)
 window.grid_rowconfigure(4, weight=1)
 
-# Label and Entry for password length
 length_label = tk.Label(window, text="Password Length :", bg="#f2f2f2")
 length_label.grid(row=0, column=0, padx=50, pady=10, sticky="w")
 length_entry = tk.Entry(window)
 length_entry.grid(row=0, column=1, padx=35, pady=0, sticky="ew")
 length_entry.insert(0, "12")
-# Checkboxes for character sets
 lowercase_var = tk.BooleanVar()
 lowercase_checkbox = tk.Checkbutton(window, text="Lowercase", variable=lowercase_var, bg="#f2f2f2")
 lowercase_checkbox.grid(row=1, column=0, padx=25, pady=10, sticky="w")
@@ -70,14 +65,11 @@ symbols_var = tk.BooleanVar()
 symbols_checkbox = tk.Checkbutton(window, text="Symbols", variable=symbols_var, bg="#f2f2f2")
 symbols_checkbox.grid(row=2, column=1, padx=25, pady=10, sticky="w")
 
-# Generate Password button
 generate_button = tk.Button(window, text="Generate Password", command=generate_password, bg="#4caf50", fg="white",
                             width=20)
 generate_button.grid(row=3, column=0, padx=10, pady=10, sticky="w")
 
-# Password entry box
 password_entry = tk.Entry(window, width=30)
 password_entry.grid(row=3, column=1, columnspan=2, padx=20, pady=0, sticky="w")
 
-# Start the GUI event loop
 window.mainloop()
